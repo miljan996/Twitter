37853bc90d584af5ac9e469125214d96295c1a80
Miljan Ignjatovic <miljan.ignjatovic.96@gmail.com>
2017-04-05 11:19:11 +0200

    Created JavaDoc for Twitter class

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -3,18 +3,33 @@ package com.twitter;
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
 /**
- * 
+ * Class used for setting user's message and reading them
  * @author Miljan Ignjatovic
+ * @version 1.0
  *
  */
 public class Twitter {
-
+	/**
+	 * Attribute that contains list of messages
+	 * Type of objects in list:
+	 * @see com.twitter.poruke.TwitterPoruka
+	 */
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
-
+	
+	/**
+	 * Get method for all messages
+	 * @return poruke - LinkedList of TwiterPoruka objects
+	 * @see com.twitter.poruke.TwitterPoruka
+	 */
 	public LinkedList<TwitterPoruka> vratiSvePoruke() {
 		return poruke;
 	}
-
+	
+	/**
+	 * Set method for user's message, message is added in list 
+	 * @param korisnik - Strings with user's name
+	 * @param poruka - String with user's message
+	 */
 	public void unesi(String korisnik, String poruka) {
 		// Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
@@ -24,7 +39,18 @@ public class Twitter {
 		// Poruka se unosi u listu na kraj
 		poruke.addLast(tp);
 	}
-
+	
+	/**
+	 * Get merthod for wanted number of messages that contains tag
+	 * @param maxBroj - Max number of messages return, if maxBroj is less then or equals 0, maxBroj is set to 100
+	 * @param tag - Tag in message
+	 * @throws java.lang.RuntimeException if:
+	 * <ul>
+	 * 		<li>tag == null</li>
+	 * 		<li>tag is empty string</li>
+	 * </ul>
+	 * @return rezultat - Array of messages	
+	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 
 		if (tag == null || tag.isEmpty())
